e7ef95e948f085c40b5984a9b0e934cc71352176
khj2023 <natista00@naver.com>
2023-12-18 14:44:14 +0900

    maximum init

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         Josephus_permuation jp = new Josephus_permuation();
-        jp.solution();
+        //jp.solution();
+        Seek_the_MaximumValue_Index smi = new Seek_the_MaximumValue_Index();
+        smi.solution();
 
     }
 }
---
+++ src/Seek_the_MaximumValue_Index.java
@@ -0,0 +1,6 @@
+public class Seek_the_MaximumValue_Index {
+
+    void solution(){
+        System.out.println("smi");
+    }
+}
