161776596f322a068ea2045c222688a121be25e2
khj2023 <natista00@naver.com>
2023-12-18 20:56:09 +0900

    seek index solution 3

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
         Josephus_permuation jp = new Josephus_permuation();
         //jp.solution();
         Seek_the_MaximumValue_Index smi = new Seek_the_MaximumValue_Index();
-        smi.solution();
+        int[] matter = {3,5,4,6,3,3,6,5};
+        int[] answer = smi.solution1(matter);
 
     }
 }
