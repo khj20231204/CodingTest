13515dc7522a858d69366e5472242f74896f15ac
khj2023 <natista00@naver.com>
2024-02-09 01:08:29 +0900

    MapShortestDistance

--- src/ch4/set_ex/_4_MapShortestDistance.java
+++ src/ch4/set_ex/_4_MapShortestDistance.java
@@ -11,27 +11,59 @@ public class _4_MapShortestDistance {
         boolean equals(Location l){
             return this.x == l.x && this.y == l.y;
         }
+
+        Location left(){
+            return new Location(x-1,y);
+        }
+
+        Location right(){
+            return new Location(x+1,y);
+        }
+
+        Location up(){
+            return new Location(x, y+1);
+        }
+
+        Location down(){
+            return new Location(x,y-1);
+        }
     }
 
     class Position{
         int steps;
-        position;
+        Location location;
+        Position(Location l, int s){
+            location = l; steps = s;
+        }
+
+        Location left(){
+            return new Location(x-1,y);
+        }
     }
 
     public int solution(int[][] maps) {
 
-        target = maps.length,
-        Queue<Position> queue = new LinkedList<>();
-        queue.add();
+        int mapSizeX = maps.length;
+        int mapSizeY = maps[0].length;
 
+        Location target = new Location(maps.length, maps[maps.length-1].length);
+        Queue<Position> queue = new LinkedList<>();
+
+        queue.add(new Position(new Location(0,0),1));
         while(!queue.isEmpty()){
             Position now = queue.poll();
 
-            if(now.position == target){
+            //벽, 맵 밖, 중복은 체크 안한다
+
+
+            if(now.location.equals(target)){
                 return now.steps;
             }
 
-            queue.offer();
+            queue.offer(new Position(now.location.left(), now.steps+1));
+            queue.offer(new Position(now.location.right(), now.steps+1));
+            queue.offer(new Position(now.location.up(), now.steps+1));
+            queue.offer(new Position(now.location.down(), now.steps+1));
         }
         return -1;
     }
