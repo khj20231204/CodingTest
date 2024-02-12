6e3b8a31586f5726e01ae2fbf4a1c144901bdfe3
khj2023 <natista00@naver.com>
2024-02-12 15:38:21 +0900

    stringCompare

--- src/ch8/BFS_DFS_ex/StringCompare.java
+++ src/ch8/BFS_DFS_ex/StringCompare.java
@@ -4,26 +4,32 @@ import java.util.*;
 
 public class StringCompare {
     public int solution(String s1, String s2){
+        /*
+        s1과 s2의 글자 길이가 같다
+        "aaobbg"와 "aoffbc" -> 같은 문자 소거 후 abg 외 ffc가 남음
+        "dog" 와 tto" -> 같은 문자 소거 후 dg와 tt가 남음
+         */
 
+        if(s1.length() != s2.length()) return 0;
+
+        return 0;
+    }
+
+    String checkChars(String s1, String s2){
         char[] s1CharArr = s1.toCharArray();
         char[] s2CharArr = s2.toCharArray();
 
-        Queue<Character> q = new LinkedList<>();
         int count=0;
+        Queue<Character> s1Q = new LinkedList<>();
+        Queue<Character> s2Q = new LinkedList<>();
         for(int i=0 ; i<s1CharArr.length ; i++){
 
-            q.offer(s1CharArr[i]);
-            boolean check = false;
-            while(!q.isEmpty()){
+            for(int j=0 ; j<s2CharArr.length ; j++){
 
-                char c = (char)q.poll();
-                for(int j=0 ; j<s2CharArr.length ; j++){
-                    if(c != (char)s2CharArr[j]);
-                }
             }
 
         }
 
-        return 0;
+        return "";
     }
 }
--- src/ch8/BFS_DFS_ex/ch8_Main.java
+++ src/ch8/BFS_DFS_ex/ch8_Main.java
@@ -12,7 +12,17 @@ public class ch8_Main {
 
         int[][] computers = {{1,1,0},{1,1,0},{0,0,1}};
 
+        for(int i=0 ; i<2 ; i++){
+            System.out.println("i:"+i);
+            for(int j=0 ; j<2 ; j++){
+                System.out.println("j:"+j);
+                for(int s=0 ; s<6 ; s++){
+                    if(s==2) break;
 
+                    System.out.println("s:"+s);
+                }
+            }
+        }
 
     }
 }
