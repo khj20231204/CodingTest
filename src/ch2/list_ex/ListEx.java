8bc529b1b68e17aaa4485c66a544bd1cfe3b4944
khj2023 <natista00@naver.com>
2024-02-24 18:46:31 +0900

    hatesameNumber

--- src/ch2/list_ex/ListEx.java
+++ src/ch2/list_ex/ListEx.java
@@ -52,7 +52,18 @@ public class ListEx {
       str.add(MyData.create("Brown"));
       str.add(new MyData("tom"));
 
-      System.out.println(str.toString());
-      System.out.println(str.contains(new MyData("tom")));
+      str.toString(); //[Brown , tom ]
+      str.contains(new MyData("tom")); //true
+
+      //list는 get안에 idx기 때문에 이렇게 못 가져온다
+      //str.get(new MyData("tom")) //error발생
+
+      //list에서 저장된 데이터를 가져오기 위해서 indexOf로 index를 가져온 후
+      //get안에 idx를 넣는다.
+      int idx = str.indexOf(new MyData("tom")); //1
+      MyData data = str.get(idx);
+      System.out.println(data.name); //tom
+
+
    }
 }
