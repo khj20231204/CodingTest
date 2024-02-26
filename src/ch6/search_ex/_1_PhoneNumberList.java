f6c31694bee27af5dfb9ed47680d6b91c0886eea
khj2023 <natista00@naver.com>
2024-02-26 17:17:43 +0900

    ch6_linearSearch

--- src/ch6/search_ex/_1_PhoneNumberList.java
+++ src/ch6/search_ex/_1_PhoneNumberList.java
@@ -3,13 +3,5 @@ package ch6.search_ex;
 import java.util.*;
 
 public class _1_PhoneNumberList {
-    public boolean solution(String[] book) {
-        boolean answer = true;
 
-        Arrays.sort(book);
-        for(int i=0 ; i<book.length-1 ; i++){
-            if(book[i+1].startsWith(book[i])) return false;
-        }
-        return true;
-    }
 }
