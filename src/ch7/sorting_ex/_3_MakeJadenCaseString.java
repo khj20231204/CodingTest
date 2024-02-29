bfec187cbc4b13e5a69acbe00474de685c1ea589
khj2023 <natista00@naver.com>
2024-03-01 01:42:48 +0900

    jadenCase

--- src/ch7/sorting_ex/_3_MakeJadenCaseString.java
+++ src/ch7/sorting_ex/_3_MakeJadenCaseString.java
@@ -1,5 +1,25 @@
 package ch7.sorting_ex;
 
 public class _3_MakeJadenCaseString {
+    public String solution(String s) {
+        String answer = "";
 
+        s = s.toLowerCase();
+
+        char[] charArr = new char[s.length()];
+
+        char before = ' ';
+        int cnt = 0;
+        for(char c : s.toCharArray()){
+            if(before == ' ') c = Character.toUpperCase(c);
+
+            charArr[cnt++] = c;
+            before = c;
+        }
+
+        answer = String.valueOf(charArr);
+
+
+        return answer;
+    }
 }
