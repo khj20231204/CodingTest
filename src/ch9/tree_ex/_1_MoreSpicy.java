e4f5e39f91c49de67dc5258adc8c35636dc28840
khj2023 <natista00@naver.com>
2024-03-05 21:22:42 +0900

    morespicy

--- src/ch9/tree_ex/_1_MoreSpicy.java
+++ src/ch9/tree_ex/_1_MoreSpicy.java
@@ -3,59 +3,48 @@ package ch9.tree_ex;
 import java.util.*;
 
 public class _1_MoreSpicy {
+   /* 정확성 테스트는 통과 , 효율성 테스트는 실패 */
+   public int solutionList(int[] scoville, int K) {
 
-   public int solution(int[] scoville, int K) {
+      List<Integer> list = new LinkedList<>();
+      for(int s : scoville) list.add(s);
+
+      Collections.sort(list);
+
       int answer = 0;
-
-      //List<Integer> list = Arrays.stream(scoville).boxed().collect(Collectors.toList());
-
-      PriorityQueue<Integer> heap = new PriorityQueue<>();
-      for(int i : scoville){
-         heap.offer(i);
-      }
-
-      int kCheck = 0;
-
-      while(heap.size() >=2) {
-         int s1 = heap.poll();
-         int s2 = heap.poll();
-
+      while(list.get(0) < K && list.size() >= 2){
+         int s1 = list.remove(0);
+         int s2 = list.remove(0);
          int s3 = s1 + s2*2;
-
-
-         //if(heap.peek() >= K) break;
-
-         heap.offer(s3);
+         list.add(s3);
          answer++;
-         if(s3 >= K) break;
 
+         Collections.sort(list);
       }
 
-      if(heap.peek() < K) return -1;
+      if(list.get(0) < K) return -1;
 
       return answer;
-        /*
-        while(heap.size()>=2){
-
-            int one = heap.poll();
-            int two = heap.poll();
-
-            kCheck = one + two*2;
-            //System.out.println("kCheck:"+kCheck);
-            answer++;
-
-            if(kCheck >= K) break;
-
-            heap.offer(kCheck);
-
-        }
-
-        if(kCheck >= K) return answer;
-
-
-        return -1;*/
    }
 
+   /* 효율성 테스트도 통과 */
+   public int solutionPriorityQueue(int[] scoville, int K) {
+      PriorityQueue<Integer> q = new PriorityQueue<>();
+      for(int s : scoville) q.offer(s);
+
+      int answer = 0;
+      while(q.peek() < K && q.size() >= 2){
+         int s1 = q.poll();
+         int s2 = q.poll();
+         int s3 = s1 + s2*2;
+         q.offer(s3);
+         answer++;
+      }
+
+      if(q.peek() < K) return -1;
+
+      return answer;
+   }
    public int Mysolution_Error(int[] scoville, int K) {
       int answer = 0;
 
