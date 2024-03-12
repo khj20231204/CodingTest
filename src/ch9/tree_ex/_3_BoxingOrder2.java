cbee68427600f65383c8c88a4959bcc3e0e2b135
khj2023 <natista00@naver.com>
2024-03-12 15:54:35 +0900

    boxer

--- src/ch9/tree_ex/_3_BoxingOrder2.java
+++ src/ch9/tree_ex/_3_BoxingOrder2.java
@@ -1,7 +1,7 @@
 package ch9.tree_ex;
 import java.util.*;
 public class _3_BoxingOrder2 {
-   class Node{
+   private class Node{
       int v;
       boolean visited = false;
       int win = 0;
@@ -74,5 +74,77 @@ public class _3_BoxingOrder2 {
    }
    void makeVisitedFalse(List<Node> list){
       for(Node no : list) no.visited = false;
+   }
+
+   //-------------------------------------
+
+
+   private class Boxer{
+      int v;
+      boolean visited = false;
+      int win = 0;
+      int lose = 0;
+      List<Boxer> list;
+
+
+      public Boxer(int v){
+         this.v = v;
+         list = new ArrayList<>();
+      }
+   }
+
+
+   public int solution2(int n, int[][] results) {
+
+      //node생성
+      List<Boxer> boxerList = new ArrayList<>(); //아무의미 없이 Boxer를 연결하기 위해 사용
+      for(int i=0 ; i<n ; i++){
+         boxerList.add(new Boxer(i+1)); //Boxer에 입력은 값으로
+      }
+
+      //연결
+      for(int[] i : results){
+         Boxer winner = boxerList.get(i[0]-1); //boxerList에서 불러올 땐 idx로
+         Boxer loser = boxerList.get(i[1]-1);
+
+         winner.list.add(loser); //연결만 되어 있으면 loser가 된다
+      }
+
+      //탐색
+      for(Boxer winner : boxerList){
+         //visited를 false로
+         makeVisitedFalse2(boxerList);
+
+         Queue<Boxer> q = new LinkedList<>();
+         q.offer(winner);
+
+         while(!q.isEmpty()){
+            Boxer j = q.poll();
+            System.out.println("winner.v:"+winner.v+" ,winner.win:"+winner.win+" ,winner.lose:"+winner.lose);
+            for(Boxer i : j.list){
+               if(i.visited) continue;
+
+               winner.win += 1 ;
+               i.lose += 1;
+               i.visited = true;
+
+               q.offer(i);
+            }
+         }
+
+      }
+
+      int answer = 0;
+      for(Boxer b : boxerList){
+         if(b.win+b.lose == (n-1)) answer++;
+      }
+
+      return answer;
+   }
+
+   void makeVisitedFalse2(List<Boxer> boxerList){
+      for(Boxer b : boxerList){
+         b.visited = false;
+      }
    }
 }
