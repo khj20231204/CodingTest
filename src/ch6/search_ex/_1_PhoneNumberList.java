90bccdc7ca30608bfd0f01358e616d0afdc9e581
khj2023 <natista00@naver.com>
2024-03-24 23:31:52 +0900

    phonenumber

--- src/ch6/search_ex/_1_PhoneNumberList.java
+++ src/ch6/search_ex/_1_PhoneNumberList.java
@@ -3,5 +3,27 @@ package ch6.search_ex;
 import java.util.*;
 
 public class _1_PhoneNumberList {
+    public boolean solution(String[] book) {
+        boolean answer = true;
+
+        Arrays.sort(book);
+        for(int i=0 ; i<book.length-1 ; i++){
+            if(book[i+1].startsWith(book[i])) return false;
+        }
+        return true;
+    }
+    public boolean solution2(String[] phone_book) {
+        boolean answer = true;
+
+        Arrays.sort(phone_book);
+
+        String before = "1"+phone_book[0];
+        for(String s : phone_book){
+            if(s.startsWith(before)) return false;
+            before = s;
+        }
+
+        return true;
+    }
 
 }
