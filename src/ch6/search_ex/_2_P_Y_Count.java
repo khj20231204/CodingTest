c6e462555b8504ea5ea9a8ca25226520f7d6f8e0
khj2023 <natista00@naver.com>
2024-03-25 00:09:20 +0900

    pycount

--- src/ch6/search_ex/_2_P_Y_Count.java
+++ src/ch6/search_ex/_2_P_Y_Count.java
@@ -1,25 +1,5 @@
 package ch6.search_ex;
 
 public class _2_P_Y_Count {
-    boolean solution1(String s) {
-        String sUpper = s.toUpperCase();
 
-        int count = 0;
-        for(char c : sUpper.toCharArray()){
-            if(c == 'P'){
-                count++;
-            }else if(c == 'Y'){
-                count--;
-            }else continue;
-        }
-
-        return count == 0;
-    }
-
-    boolean solution2(String s) {
-        int p = s.replaceAll("[^pP]","").length();
-        int y = s.replaceAll("[^yY]","").length();
-
-        return p == y;
-    }
 }
