f5a1e540c0fa2ccc092ec90cc0d88f0a4bd2c087
khj2023 <natista00@naver.com>
2024-03-29 18:46:15 +0900

    targetnumber

--- src/ch8/BFS_DFS_ex/_2_TargetNumber.java
+++ src/ch8/BFS_DFS_ex/_2_TargetNumber.java
@@ -1,20 +1,5 @@
 package ch8.BFS_DFS_ex;
 
 public class _2_TargetNumber {
-    public int solution(int[] numbers, int target) {
-        int answer = sumCount(numbers, target, 0, 0);
 
-        return answer;
-    }
-
-    int sumCount(int[] numbers, int target, int idx, int sum){
-
-        if(idx >= numbers.length){
-            if(sum == target) return 1;
-            return 0;
-        }
-
-        return sumCount(numbers, target, idx+1, sum+numbers[idx])
-                + sumCount(numbers, target, idx+1, sum-numbers[idx]);
-    }
 }
