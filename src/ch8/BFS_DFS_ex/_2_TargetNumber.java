3084d8d8ffadd470ea7c0719dd170fc87fa26c85
khj2023 <natista00@naver.com>
2024-03-30 18:31:44 +0900

    wordTransform

--- src/ch8/BFS_DFS_ex/_2_TargetNumber.java
+++ src/ch8/BFS_DFS_ex/_2_TargetNumber.java
@@ -18,7 +18,11 @@ public class _2_TargetNumber {
                 + sumCount(numbers, target, idx+1, sum-numbers[idx]);
     }
 
-    //------------------ 클래스 이용 -------------------
+    /* ------------------ 클래스 이용 -------------------
+    class로 연산 결과값과 index를 저장한다. sum값을 계산 할 공간과 마지막 index값을 알아야 하기 때문에
+    일반 변수를 사용하면 저장할 공간이 없다. 클래스로 index값과 해당 index까지 연산한 결과를 계속 들고 다녀야한다.
+     */
+
     class Number {
 
         int idx, sum;
@@ -29,5 +33,27 @@ public class _2_TargetNumber {
         }
     }
 
+    public int solution2(int[] numbers, int target) {
+        int answer = 0;
+        Queue<Number> queue = new LinkedList<>();
+        queue.add(new Number(0, -numbers[0]));
+        queue.add(new Number(0, numbers[0]));
 
+        while (!queue.isEmpty()) {
+            Number cur = queue.poll();
+
+            if (cur.idx == (numbers.length - 1) && cur.sum == target) {
+                answer++;
+            }
+
+            int next = cur.idx + 1;
+            if (next < numbers.length) {
+                queue.add(new Number(next, cur.sum - numbers[next]));
+                queue.add(new Number(next, cur.sum + numbers[next]));
+            }
+
+        }
+
+        return answer;
+    }
 }
