3705bb7b840c66bba90d5111fae196f515207e92
khj2023 <natista00@naver.com>
2024-04-10 23:36:59 +0900

    dataAnalysis

--- src/progrms/codingtest_ex/dataAnalysis.java
+++ src/progrms/codingtest_ex/dataAnalysis.java
@@ -1,27 +1,23 @@
 package progrms.codingtest_ex;
 import java.util.*;
 public class dataAnalysis {
-
    class Data{
       int code;
       int date;
       int maximum;
       int remain;
-
       public Data(int code, int date, int maximum, int remain){
          this.code = code;
          this.date = date;
          this.maximum = maximum;
          this.remain = remain;
       }
-
       public String toString(){
          return "code:"+code+" ,date:"+date+" ,maximum:"+maximum+" ,remain:"+remain;
       }
    }
 
    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-
 
       List<Data> list = new ArrayList<>();
       for(int[] i : data){
@@ -52,42 +48,80 @@ public class dataAnalysis {
          }
       }
 
-      for(Data d : listSelected){
-         System.out.println(d);
-      }
-
-      /*
       listSelected.sort(new Comparator<Data>(){
          @Override
          public int compare(Data d1, Data d2){
             if(sort_by.equals("code")){
-               return d2.code - d1.code;
+               return d1.code - d2.code;
             }else if(sort_by.equals("date")){
-               return d2.date - d1.date;
+               return d1.date - d2.date;
             }else if(sort_by.equals("maximum")){
-               return d2.maximum - d1.maximum;
+               return d1.maximum - d2.maximum;
             }else{ //remain
-               System.out.println("d1.remain:"+d1.remain+" ,d2.remain:"+d2.remain);
-               return -1;
+               return d1.remain - d2.remain;
             }
          }
       });
-      */
 
-      listSelected.sort(new Comparator<Data>() {
-         @Override
-         public int compare(Data o1, Data o2) {
-            return -1;
-         }
-      });
-
-      System.out.println(listSelected.size());
       int[][] answer = new int[listSelected.size()][4];
+      int idx=0;
       for(Data d : listSelected){
-         d.toString();
-         System.out.println("d.remain:"+d.remain);
+         answer[idx][0] = d.code;
+         answer[idx][1] = d.date;
+         answer[idx][2] = d.maximum;
+         answer[idx][3] = d.remain;
+         idx++;
       }
 
       return answer;
+   }
+
+   //----------- 풀이2 --------------------------
+   public int[][] solution2(int[][] data, String ext, int val_ext, String sort_by) {
+
+      String[] arr = {"code", "date", "maximum", "remain"};
+      List<String> columnList = Arrays.asList(arr);
+      int extIdx = columnList.indexOf(ext);
+      int sortIdx = columnList.indexOf(sort_by);
+      int[][] answer = Arrays.stream(data).filter(o1 -> o1[extIdx] < val_ext)
+            .sorted((o1, o2) -> o1[sortIdx] - o2[sortIdx]).toArray(int[][]::new);
+
+      return answer;
+   }
+
+   //----------- 풀이3 --------------------------
+   public int[][] solution3(int[][] data, String ext, int val_ext, String sort_by) {
+      /*return Arrays.stream(data)
+            .filter(ints -> (ext.equals("code") ? ints[0] : ext.equals("date") ? ints[1] : ext.equals("maximum") ? ints[2] : ints[3]) < val_ext)
+            .sorted(Comparator.comparing(ints -> sort_by.equals("code") ? ints[0] : sort_by.equals("date") ? ints[1] : sort_by.equals("maximum") ? ints[2] : ints[3]))
+            .toArray(int[][]::new);*/
+
+      return Arrays.stream(data) //data : code date maximum remain
+            .filter(ss -> (ext.equals("code") ? ss[0] : ext.equals("date") ? ss[1] : ext.equals("maximum") ? ss[2] : ss[3]) < val_ext)
+            .sorted(Comparator.comparing(ints -> sort_by.equals("code") ? ints[0] : sort_by.equals("date") ? ints[1] : sort_by.equals("maximum") ? ints[2] : ints[3]))
+            .toArray(int[][]::new);
+   }
+
+   //----------- 풀이4 --------------------------
+   public List<int[]> solution4(int[][] data, String ext, int val_ext, String sort_by) {
+      List<int[]> list = new ArrayList<>();
+      String[] s = {"code", "date", "maximum", "remain"};
+      int extIdx = 0, sortIdx = 0;
+
+      for (int i = 0; i < 4; i++) {
+         if (ext.equals(s[i])) extIdx = i;
+         if (sort_by.equals(s[i])) sortIdx = i;
+      }
+      final int si = sortIdx;
+
+      for (int i = 0; i < data.length; i++) {
+         if (data[i][extIdx] < val_ext) list.add(data[i]);
+      }
+
+      // *lambda, Comparator 사용시 final 이나 effectively final여야한다!
+      Collections.sort(list, (o1, o2) -> o1[si] - o2[si]);
+
+
+      return list;
    }
 }
