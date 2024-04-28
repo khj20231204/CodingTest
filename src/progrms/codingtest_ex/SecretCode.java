24179e6040e5b56f4a60e76a27e2fb5d2528b2ab
khj2023 <natista00@naver.com>
2024-04-28 23:00:15 +0900

    secretcode

--- src/progrms/codingtest_ex/SecretCode.java
+++ src/progrms/codingtest_ex/SecretCode.java
@@ -3,23 +3,88 @@ import java.util.*;
 import java.util.stream.*;
 
 public class SecretCode {
-   public String solution(String s, String skip, int index) {
+   public String solution1(String s, String skip, int index) {
       String answer = "";
-      //소문자65~90 97~122 26개
-      //skip의 수만틈 index를 더한다
 
-      //char[] -> List<Character>
-      char[] skipChar = skip.toCharArray();
+      int[] sInt = new int[s.length()];
+      int[] skipInt = new int[skip.length()];
+      List<Integer> list = new ArrayList<>();
 
-      int[] sInt = new int[];
-      for(char c3 : skipChar)  {System.out.println((int)c3);}
-      List<Character> list = IntStream.range(0,skipChar.length).mapToObj(i->skipChar[i]).collect(Collectors.toList());
+      for(int i=0 ; i<s.length() ; i++){
+         sInt[i] = (int)s.charAt(i);
+      }
 
-      //char[] -> List<char[]>
-      List<char[]> list2 = Arrays.asList(skipChar); //contains는 가능
+      for(int i=0 ; i<skip.length() ; i++){
+         skipInt[i] = (int)skip.charAt(i);
+         list.add((int)skip.charAt(i));
+      }
 
+      //97~122
+      for(int i=0 ; i<sInt.length ; i++){
+         int count = 0;
+         for(int j=1 ; j<=index ; j++){
+            int tmp = sInt[i]+j;
+            if(tmp>122) tmp -= 26;
+            if(list.contains(tmp)) count++;
+                /*
+                for(int k=0 ; k<skipInt.length ; k++){
+                    if(tmp == skipInt[k]) count++;
+                }*/
+         }
 
+         sInt[i] = sInt[i]+count+index;
+         if(sInt[i] > 122) sInt[i] -= 26;
+      }
 
+      char[] result = new char[sInt.length];
+      for(int i=0 ; i<result.length; i++){
+         result[i] = (char)sInt[i];
+      }
+
+      answer = String.valueOf(result);
+
+      return answer;
+   }
+
+   public String solution2(String s, String skip, int index) {
+      String answer = "";
+
+      int[] sInt = new int[s.length()];
+      int[] skipInt = new int[skip.length()];
+      List<Integer> list = new ArrayList<>();
+
+      for(int i=0 ; i<s.length() ; i++){
+         sInt[i] = (int)s.charAt(i);
+      }
+
+      for(int i=0 ; i<skip.length() ; i++){
+         skipInt[i] = (int)skip.charAt(i);
+         list.add((int)skip.charAt(i));
+      }
+
+      //97~122
+      for(int i=0 ; i<sInt.length ; i++){
+         int count = 0;
+         for(int j=1 ; j<=index ; j++){
+            int tmp = sInt[i]+j;
+            if(tmp>122) tmp -= 26;
+            if(list.contains(tmp)) count++;
+                /*
+                for(int k=0 ; k<skipInt.length ; k++){
+                    if(tmp == skipInt[k]) count++;
+                }*/
+         }
+
+         sInt[i] = sInt[i]+count+index;
+         if(sInt[i] > 122) sInt[i] -= 26;
+      }
+
+      char[] result = new char[sInt.length];
+      for(int i=0 ; i<result.length; i++){
+         result[i] = (char)sInt[i];
+      }
+
+      answer = String.valueOf(result);
 
       return answer;
    }
