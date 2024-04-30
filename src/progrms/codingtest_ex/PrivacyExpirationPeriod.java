e2491ca440c0fc1f95bef66bf5974fc168291162
khj2023 <natista00@naver.com>
2024-04-30 21:42:07 +0900

    privacyExpiration

--- src/progrms/codingtest_ex/PrivacyExpirationPeriod.java
+++ src/progrms/codingtest_ex/PrivacyExpirationPeriod.java
@@ -5,29 +5,69 @@ public class PrivacyExpirationPeriod {
    public int[] solution(String today, String[] terms, String[] privacies) {
       int[] answer = {};
 
-      for(String s : privacies){
-         //System.out.println(s);
+      //terms는 set에 저장
+      Map<String, Integer> termsMap = new HashMap<>();
+      for(String s : terms){
+         String[] termsArr = s.split(" ");
+         termsMap.put(termsArr[0], Integer.valueOf(termsArr[1]));
       }
 
-      String[] str = privacies[1].split(" ");
-      for(String s : str){
-         //System.out.println(s);
+      String[] todayArrStr = today.split("\\.");
+      int[] todayIntArry = Arrays.stream(todayArrStr).mapToInt(Integer::valueOf).toArray();
+      int today_year = todayIntArry[0];
+      int today_month = todayIntArry[1];
+      int today_day = todayIntArry[2];
+
+      List<Integer> list = new ArrayList<>();
+      for(int i=0 ; i<privacies.length ; i++){
+         String s1 = privacies[i];
+
+         String[] privacy = s1.split(" ");
+         String[] date = privacy[0].split("\\."); //2021 05 02
+         int[] dateInt = Arrays.stream(date).mapToInt(Integer::valueOf).toArray();
+         int year = dateInt[0];
+         int month = dateInt[1];
+         int day = dateInt[2];
+
+         String type = privacy[1]; //A B
+         int addMonth = termsMap.get(type);
+
+         ////유효기간 만들기
+         //유효기간 month 더하기
+         month += addMonth;
+         if(month > 12){
+            year++;
+            month -= 12;
+         }
+         //day에서 1빼기
+         day--;
+         if(day < 1){
+            day = 28;
+            month--;
+            if(month < 1){
+               month = 12;
+               year--;
+            }
+         }
+
+         //약관날짜와 비교
+         boolean check = false;
+         if(year < today_year){
+            check = true;
+         }
+
+         if(year == today_year && month < today_month){
+            check = true;
+         }
+
+         if(year == today_year && month == today_month && day < today_day){
+            check = true;
+         }
+
+         if(check) list.add(i+1);
+
       }
 
-      String ss = "12";
-      int aa = Integer.valueOf(ss);
-      Integer aa2 = Integer.valueOf(ss);
-      int aa3 = Integer.parseInt(ss);
-      Integer aa4 = Integer.parseInt(ss);
-
-      //int[] -> String[]
-      int[] k = {1,2,34};
-      String[] kk = Arrays.stream(k).mapToObj(String::valueOf).toArray(String[]::new);
-
-      //String[] -> int[]
-      k = Arrays.stream(kk).mapToInt(Integer::valueOf).toArray();
-
-
-      return answer;
+      return list.stream().mapToInt(Integer::valueOf).toArray();
    }
 }
