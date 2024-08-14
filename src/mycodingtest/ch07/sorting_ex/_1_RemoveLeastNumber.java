0411c87d79e344d384acca4d12977ecd67db75c5
natista99 <natista99@gmail.com>
2024-08-14 21:25:52 +0900

    sorting_1

---
+++ src/mycodingtest/ch07/sorting_ex/_1_RemoveLeastNumber.java
@@ -0,0 +1,20 @@
+package mycodingtest.ch07.sorting_ex;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class _1_RemoveLeastNumber {
+	 public int[] solution(int[] arr) {
+        int[] answer = {};
+        
+        int len = arr.length;
+        if(len <= 1) return new int[]{-1};
+        
+        int min = Arrays.stream(arr).min().getAsInt();
+        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        
+        list.remove(list.indexOf(min));
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
